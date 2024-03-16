af86016563cfcfd3f012ec6e0df56813c86b8ae7
MUHAMMAD BINTANG RIZKY <bintangrizky2110@gmail.com>
2024-03-16 23:28:38 +0700

    Tugas day 19

---
+++ Tugas MSIB Day 19/script.js
@@ -0,0 +1,96 @@
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+});
+
+
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Lihat Resep</a>
+                        </div>
+                    </div>
+                `;
+            });
+            mealList.classList.remove('notFound');
+        } else{
+            html = "Maaf, Kami Ga Nemunin Apa Yang Kamu Cari 😔";
+            mealList.classList.add('notFound');
+        }
+
+        mealList.innerHTML = html;
+    });
+}
+
+
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
+    }
+}
+
+
+function mealRecipeModal(meal){
+    console.log(meal);
+    meal = meal[0];
+    let html = `
+        <h2 class="recipe-title">${meal.strMeal}</h2>
+        <p class="recipe-category">${meal.strCategory}</p>
+        <div class="recipe-instruct">
+            <h3>Instruksi:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class="recipe-ingredients">
+            <h3>Ingredients / Bahan-bahan:</h3>
+            <p>
+    `;
+
+    const ingredientsArray = [];
+
+    for (let i = 1; i <= 20; i++) {
+        const ingredient = meal[`strIngredient${i}`];
+        const measure = meal[`strMeasure${i}`];
+
+        if (ingredient && ingredient.trim() !== '') {
+            ingredientsArray.push(`${ingredient} - ${measure}`);
+        }
+    }
+
+    html += ingredientsArray.join(', ');
+
+    html += `
+            </p>
+        </div>
+        <div class="recipe-meal-img">
+            <img src="${meal.strMealThumb}" alt="">
+        </div>
+        <div class="recipe-link">
+            <a href="${meal.strYoutube}" target="_blank">Lihat Video</a>
+        </div>
+    `;
+
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
+}
