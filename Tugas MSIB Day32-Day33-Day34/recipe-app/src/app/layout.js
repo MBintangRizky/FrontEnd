017f8e44c476226923d83d0baf39c7171f9f862c
MUHAMMAD BINTANG RIZKY <bintangrizky2110@gmail.com>
2024-04-03 22:23:05 +0700

    Tugas day 32, day 33, day 34

---
+++ Tugas MSIB Day32-Day33-Day34/recipe-app/src/app/layout.js
@@ -0,0 +1,41 @@
+import { Inter, Roboto, Poppins } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+const inter = Inter({ 
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: '--font-inter', //css variables name
+})
+
+const roboto = Roboto({ 
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  variable: '--font-roboto',
+})
+
+const poppins = Poppins({ 
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  variable: '--font-poppins',
+})
+
+export const metadata = {
+  title: "Recipe App",
+  description: "Aplikasi pencari resep",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${roboto.variable} ${poppins.variable}`}>
+        <div className="container">
+          <Navbar/>
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
